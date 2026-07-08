9a448d0ec940144dea6d799cf503fdb856c48fec
vy286 <vy251028@gmail.com>
2026-07-08 09:41:30 +0700

    Them Morgan logger

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+   const morgan = require('morgan'); // Khai báo morgan
+
    const app = express();
    const port = 3000;
+
+   // Tích hợp morgan vào app
+   app.use(morgan('combined'));
 
    app.get('/', (req, res) => {
      res.send('Chào mừng đến với Blog cá nhân của tôi!');
