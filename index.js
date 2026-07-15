cb2d159240a3f56eb8d3ef78c87224f294c99726
vy286 <vy251028@gmail.com>
2026-07-15 08:18:58 +0700

    Hoan thanh Routing va Query Parameters

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
-   const morgan = require('morgan'); // Khai báo morgan
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 
-   const app = express();
-   const port = 3000;
+const app = express();
+const port = 3000;
 
-   // Tích hợp morgan vào app
-   app.use(morgan('combined'));
+app.use(morgan('combined'));
 
-   app.get('/', (req, res) => {
-     res.send('Chào mừng đến với Blog cá nhân của tôi!');
+// Cấp quyền truy cập cho thư mục public
+app.use(express.static('public'));
+
+// Cấu hình Handlebars
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+// Route xử lý cho trang About
+   app.get('/about', (req, res) => {
+       res.render('about');
    });
 
-   app.listen(port, () => {
-     console.log(`Server đang chạy tại http://localhost:${port}`);
+   // Route xử lý cho trang Liên hệ
+   app.get('/contact', (req, res) => {
+       res.render('contact');
    });
+
+   // Route hiển thị trang Tìm kiếm
+   app.get('/search', (req, res) => {
+    console.log("Từ khóa tìm kiếm:", req.query.q);
+    console.log("Loại tìm kiếm:", req.query.type);
+       res.render('search');
+   });
+
+app.listen(port, () => {
+  console.log(`Server đang chạy tại http://localhost:${port}`);
+});
